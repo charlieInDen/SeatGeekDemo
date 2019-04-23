b1261dbc0d4dbbd466324f2fc7c5d6dece91105a
Nishant Sharma <nsharma@ivycomptech.com>
2019-04-23 11:44:04 +0530

    initial commit

---
+++ SeatGeekDemo/Extensions/Image+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Image+Extension.swift
+//  SeatGeekDemo
+//
+//  Created by Nishant Sharma on 21/04/2019
+//  Copyright © 2019 Nishant Sharma. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    class func alwaysRendering(named: String) -> UIImage? {
+        return UIImage(named: named)?.withRenderingMode(.alwaysTemplate)
+    }
+}
